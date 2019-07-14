dfe376a6b5fe73ae398520b1c770908144c53d0d
Andre Pestana <andre.pestana@skyone.solutions>
2019-07-14 14:48:06 -0300

    Adding initial imports in service

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,19 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.NetworkException
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.Customer
+import io.pleo.antaeus.data.AntaeusDal
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val paymentProvider: PaymentProvider,
+    private val dal: AntaeusDal
 ) {
-   // TODO - Add code e.g. here
+   fun charge(invoice: Invoice): Boolean {
+       
+       return true
+   }
 }
